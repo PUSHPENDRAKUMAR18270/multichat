a2b0d30a2a12e5b9f0805d3079f004d3a69e71d6
PUSHPENDRAKUMAR18270 <53979418+PUSHPENDRAKUMAR18270@users.noreply.github.com>
2020-11-11 10:59:31 +0530

    update

--- multichat/server.java
+++ multichat/server.java
@@ -1,72 +1,144 @@
-import java.io.*;
 import java.net.*;
+import java.io.*;
+import java.lang.Thread;
+import java.util.*;
 
-class B1aServer3336{
+class User extends Thread{
+	private Socket currsoc;
+	private String username;
+	private DataInputStream din;
+	private DataOutputStream dout;
 
-	final int portNumber=10000;
-	public void formSession() throws IOException
+	public User(Socket soc) throws IOException
 	{
-		//opens a port
-		ServerSocket port = new ServerSocket(portNumber);
-		System.out.println("Waiting for clients");
-		//form a connection between server and client at port when it recieves a client request
-		Socket soc = port.accept(); 
-		//streams	
-		BufferedReader input;
-		DataInputStream din;
-		DataOutputStream dout;
-		try
-		{
-			//user input stream
-			input=new BufferedReader(new InputStreamReader(System.in));
-			//stream to read from socket
-			din =new DataInputStream(soc.getInputStream());
-			//stream to write to socket
-			dout =new DataOutputStream(soc.getOutputStream());
+		this.currsoc=soc;
+		din=new DataInputStream(soc.getInputStream());
+		dout =new DataOutputStream(soc.getOutputStream());
+		username=din.readUTF();
+		//get status of user
+		onlineStatus();
+		//activate thread;
+		this.start();
+	}
+
+	public void run() 
+	{
+		try{
+			chat();
 		}
 		catch(Exception e)
 		{
-			System.out.println("error in stream formation");
+			e.printStackTrace();
 			return;
 		}
-		//loop goes on till connection
-		while(true)
-		{	
-			//read message from socket 
-			String str;
-			str=din.readUTF();
-			System.out.println("from client: "+str);
-			if(str.equals("bye"))
-			{
-				dout.writeUTF("bye");
-				break;
+		finally{
+			try{
+				din.close();
+				dout.close();
+				currsoc.close();
+				return;
 			}
-			System.out.print("to client: ");
-			//read a string 
-			str=input.readLine();
-			System.out.println(str);
-			//write to socket
-			dout.writeUTF(str);
-			
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
 		}
-		//close the socket
-		soc.close();
-		//close streams
-		input.close();
-		din.close();
-		dout.close();
 	}
 
-	public static void main(String[] agrs)
-	{
-		B1aServer3336 obj=new B1aServer3336();
-		try
+	public void onlineStatus() throws IOException
+	{		
+		if(B1bServer3336.mp.containsKey(currsoc)==false)
 		{
-			obj.formSession();
+			B1bServer3336.mp.put(currsoc,username);
+			dout.writeUTF(B1bServer3336.mp.get(currsoc)+" is Online");
 		}
-		catch(IOException e)
+		else{
+			dout.writeUTF(B1bServer3336.mp.get(currsoc)+" is already Online");
+		}
+	}
+
+	public void broadcast(String msg) throws IOException
+	{
+		for(Socket soc:B1bServer3336.mp.keySet())
 		{
-			System.out.println("error in i/o");
+			if(currsoc==soc){
+				continue;
+			}
+			DataOutputStream td=new DataOutputStream(soc.getOutputStream());
+			td.writeUTF(username+":"+msg);
+		}
+	}
+
+	public void chat() throws IOException
+	{
+		//read from user	
+		while(true)
+		{
+			//read a message from client
+			String msg=null;
+			if(din.available()>0)
+				msg=din.readUTF();
+			if(msg!=null&&B1bServer3336.mp.containsKey(currsoc))
+			{
+				if(B1bServer3336.mp.size()>1)
+				{
+					if(msg.equals("leave"))
+					{
+						msg=B1bServer3336.mp.get(currsoc)+" left the chat";
+						B1bServer3336.mp.remove(currsoc);
+						broadcast(msg);						
+						return;
+					}
+					else {
+						broadcast(msg);
+					}
+				}
+				else if(B1bServer3336.mp.size()==1)
+				{
+					if(msg.equals("leave"))
+					{
+						B1bServer3336.mp.remove(currsoc);
+						currsoc.close();
+					}
+					else
+						dout.writeUTF("there is no one online");
+				}
+			}
+			if(B1bServer3336.mp.size()==0)
+			{
+				System.out.println("there is no one in chat!!");
+				return;
+			}
+		}
+		
+	}
+}
+
+
+
+class B1bServer3336{
+	final static int portnumber=10000;
+	public static Map<Socket,String>mp = new HashMap<>();
+	public static void main(String[] args) throws IOException
+	{
+		ServerSocket port;
+		//open a port
+		port = new ServerSocket(portnumber,3);
+		System.out.println("waiting for clients");
+		while(true)
+		{
+			//accept a connection
+			Socket soc=port.accept();
+			System.out.println("new user detected");
+			//create a thread for newuser contacting server
+			try{
+				User newUser=new User(soc);
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+			
 		}
 	}
 }
