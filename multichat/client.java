5a64ea605ba997fadbb8b560844c0aaf29f05f02
PUSHPENDRAKUMAR18270 <53979418+PUSHPENDRAKUMAR18270@users.noreply.github.com>
2020-11-11 10:58:48 +0530

    update

--- multichat/client.java
+++ multichat/client.java
@@ -1,67 +1,128 @@
 import java.io.*;
 import java.net.*;
+import java.lang.Thread;
 
-class B1aClient3336{
 
-	final int port=10000;
 
-	public void formSession() throws IOException
-	{
-		//create a socket to connect to server accepts serverip and serverport;
-		Socket serverConnection= new Socket("127.0.0.1",port);
-		//streams
-		BufferedReader input;
-		DataInputStream din;
-		DataOutputStream dout;
-		
-		try
-		{
-			//user input stream
-			input=new BufferedReader(new InputStreamReader(System.in));
-			//stream to read from socket i.e server
-			din =new DataInputStream(serverConnection.getInputStream());
-			//stream to write to socket i.e server
-			dout =new DataOutputStream(serverConnection.getOutputStream());
-	    }
-	    catch(Exception e)
-	    {
-	    	System.out.println("error in creating streams");
-	    	return;
-	    }
-	
-		//loop goes on till connection
-		while(true)
-		{		
-			System.out.print("to Server: ");
-			//read a string 
-			String str=input.readLine();
-			System.out.println(str);		
-			//write to socket
-			dout.writeUTF(str);	
-			//read message from socket
-			str=din.readUTF();
-			System.out.println("from server: "+str);
-			if(str.equals("bye"))
-			{
-				break;
-			}
-		}
-		//close the socket
-		serverConnection.close();
-		//close streams
-		input.close();
-		din.close();
-		dout.close();
-	}
-	public static void main(String[] agrs)
-	{
-		B1aClient3336 obj =new B1aClient3336();
-		try{
-			obj.formSession();
-		}
-		catch(IOException e)
-		{
-			System.out.println("error in i/o");
-		}
-	}
+public class B1bClient3336{
+
+    private final static int port = 10000;
+    private  String username;
+    private Socket cltSocket;
+    private DataOutputStream dout;
+    private DataInputStream din;
+    private BufferedReader userInput;
+    Thread t1,t2;
+    //inner class
+    class sendThread extends Thread{
+
+        @Override
+        public void run() {
+            try{
+                send();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+            
+        }
+
+        //send function
+        void send() throws IOException{
+            
+            while(true){
+                // System.out.println("say what you want to write");
+                String str = userInput.readLine();
+                if(str.length()>0)
+                {
+                    dout.writeUTF(str);
+                    if(str.equals("leave")){
+                        {
+                            dout.close();
+                            din.close();
+                            cltSocket.close();
+                            System.exit(0);
+                            return;
+                        }
+                    } 
+                }          
+            }
+        }
+
+    }
+
+    //inner class
+    class recvThread extends Thread{
+        @Override
+        public void run() {
+            try{
+                recv();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+            
+            
+        }
+
+
+        void recv() throws IOException{
+            while(true){
+                //returns an estimate no of bytes to be read
+                if(din.available()>0)
+                { 
+                    // System.out.println(din.available());
+                    String str = din.readUTF();
+                    String token[] = str.split(":");
+                     if(token.length==2){
+                         System.out.println(token[0]);
+                         System.out.println("  -->"+token[1]);
+                     }
+                     else{
+                         System.out.println(token[0]);
+                     } 
+                }       
+            }
+        }
+
+    }
+
+
+    public void newClient() throws IOException
+    {
+        System.out.println("[+] Client Started...");
+        //establish connection to server on port 10000
+        cltSocket = new Socket("localhost",port);          
+        System.out.println("[+] Connection established...");
+                
+        dout = new DataOutputStream(cltSocket.getOutputStream());
+        din =  new DataInputStream(cltSocket.getInputStream());
+        userInput = new BufferedReader(new InputStreamReader(System.in));  
+        System.out.print("Enter username: ");
+        username = userInput.readLine();
+        System.out.println(username);
+        dout.writeUTF(username);
+        t1 = this.new sendThread();
+        t2= this.new recvThread();
+        t1.start();  
+        t2.start();
+        // cltSocket.close();
+        // din.close();
+        // dout.close();
+        // cltSocket.close();
+    }
+
+    public static void main(String []args) throws IOException,InterruptedException
+    {
+        B1bClient3336 obj=new B1bClient3336();
+        try{
+            obj.newClient();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            return;
+        }
+    }
+
 }
